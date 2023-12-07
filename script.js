1bcf257345701f0f7226b47e1d07cc0fcf738f43
AyushChaursiaya <147719677+AyushChaursiaya@users.noreply.github.com>
2023-12-07 23:27:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
 function isSameType(value1, value2) {
-  //your js code here
-	if (isNaN(value1) && isNaN(value2)) {
-		return true;
-	}
-	return typeof value1 === typeof value2;
+  if (isNaN(value1) && isNaN(value2)) {
+    return true;
+  }
+
+  return typeof value1 === typeof value2;
 }
 
-console.log(isSameType(1, 3));
-console.log(isSameType("hey", "hello"));
-console.log(isSameType(NaN, NaN));
-console.log(isSameType("3", 3));
-// do not change the code below.
-let value1 = prompt("Enter Start of the Range.");
-let value2 = prompt("Enter End Of the Range.");
-alert(isSameType(value1, value2));
+function checkSameType() {
+  const value1 = document.getElementById("value1").value;
+  const value2 = document.getElementById("value2").value;
+  const resultElement = document.getElementById("result");
+
+  // Convert input values to numbers if they are numeric strings
+  const numValue1 = isNaN(value1) ? value1 : parseFloat(value1);
+  const numValue2 = isNaN(value2) ? value2 : parseFloat(value2);
+
+  const result = isSameType(numValue1, numValue2);
+
+  resultElement.textContent = result ? "Same type" : "Different types";
+}
